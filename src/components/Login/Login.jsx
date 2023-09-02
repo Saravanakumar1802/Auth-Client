9dc930477685422b9f632a9828f771b91797362b
Saravanakumar1802 <saravana1802kumar@gmail.com>
2023-09-02 17:51:56 +0530

    fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../Login/Login.css'
+import '../Login/login.css'
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
